1f8297b5237d0d25145d81c20949c3da51c97733
Wen Yi <wen-yi@qq.com>
2024-08-22 05:59:14 +0800

    Imporve format

--- src/cmd_admin.h
+++ src/cmd_admin.h
@@ -37,7 +37,7 @@ class CmdConfig : public BaseCmdGroup {
  private:
   //  std::vector<std::string> subCmd_;
 
-  void DoCmd(PClient* client) override {};
+  void DoCmd(PClient* client) override{};
 };
 
 class CmdConfigGet : public BaseCmd {
@@ -141,7 +141,7 @@ class CmdDebug : public BaseCmdGroup {
   bool DoInitial(PClient* client) override { return true; };
 
  private:
-  void DoCmd(PClient* client) override {};
+  void DoCmd(PClient* client) override{};
 };
 
 class CmdDebugHelp : public BaseCmd {
