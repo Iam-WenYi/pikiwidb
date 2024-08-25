accbb1c84b83f74a979dc1f15c48604d83b8a9ba
Wen Yi <wen-yi@qq.com>
2024-08-25 22:04:34 +0800

    Imporve code format

--- src/pikiwidb.cc
+++ src/pikiwidb.cc
@@ -127,15 +127,12 @@ bool PikiwiDB::ParseArgs(int argc, char* argv[]) {
       }
       case 's': {
         unsigned int optarg_long = strlen(optarg);
-        char *str = calloc(optarg_long, sizeof(char*));
-        if (str)
-        {
+        char* str = calloc(optarg_long, sizeof(char*));
+        if (str) {
           sscanf(optarg, "%s:%d", str, master_port_);
           master_ = str;
           free(str);
-        }
-        else
-        {
+        } else {
           ERROR("Memory alloc failed.");
         }
         break;
